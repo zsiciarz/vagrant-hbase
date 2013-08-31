b40669121820855010d38f1dee9471c385d74082
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-31 12:07:42 +0200

    Move value3 to another column in the same CF.

--- data/create_test_table.rb
+++ data/create_test_table.rb
@@ -1,4 +1,4 @@
 create 'test', 'cf'
 put 'test', 'row1', 'cf:col1', 'value1'
 put 'test', 'row2', 'cf:col1', 'value2'
-put 'test', 'row3', 'cf:col1', 'value3'
+put 'test', 'row3', 'cf:col2', 'value3'
